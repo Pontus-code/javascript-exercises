97aa5d30c0b0e75534f1921ed8e938b71893a2f7
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2025-06-07 23:46:49 +1200

    Add solution 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(p1, p2) {
 
+    let lowest = p1
+    let highest = p2
+
+    if (lowest > highest) {
+        lowest = p2
+        highest = p1
+    }
+
+    if (lowest < 0 || highest < 0) {
+        return "ERROR"
+    }
+
+    if (typeof(lowest) != "number" || typeof(highest) != "number") {
+        return "ERROR"
+    }
+
+    if (lowest != lowest.toFixed(0) || highest != highest.toFixed(0)) {
+        return "ERROR"
+    }
+
+
+
+
+    let result = 0
+    for (let i = lowest; i < highest+1; i++) {
+        result = result + i
+    }
+    return result
 };
 
 // Do not edit below this line
