35e307368bc78da42d7e1733984d36729a2cc3c5
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2025-06-07 23:19:53 +1200

    Add solution 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(p1,p2,p3,p4,p5) {
+    let array = p1
+    let toRemove = [p2,p3,p4,p5]
+    let newArray = []
 
+    for (let i = 0; i < array.length; i++) {
+        removeMe = ""
+        for (let j = 0; j < toRemove.length; j++) {
+            if (array[i] === toRemove[j]) {
+                removeMe = "yes"
+            } 
+        }
+        if (removeMe != "yes") {
+            newArray.push(array[i])
+        }
+    }
+    return newArray
+}
 // Do not edit below this line
 module.exports = removeFromArray;
