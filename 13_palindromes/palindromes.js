784cde417619bed133ff57308d86a3648a914d28
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2025-06-08 14:35:35 +1200

    Add solution 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
-};
+    let newString = string.replace(/\s+/ig, "") // Remove white spaces
 
+    newString = newString.replace(/\p{P}/gu, "") // Remove punctuations
+
+    newString = newString.toUpperCase()
+
+
+    arrayString = newString.split("")
+
+    for (let i = 0; i < arrayString.length; i++) {
+        console.log("Element number " + i +" is " + arrayString[i] + "\nand element number " + (arrayString.length - 1 - i) + " is " + arrayString[arrayString.length - 1 - i])
+        if (arrayString[i] != arrayString[arrayString.length - 1 - i]) {
+            return false
+        }
+    }
+    return true
+}
 // Do not edit below this line
 module.exports = palindromes;
